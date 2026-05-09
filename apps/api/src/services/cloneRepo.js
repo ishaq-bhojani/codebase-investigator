8d9bdc8deb1ce2d2d1896ec319ad6169eb5de13f
Ishaq Bhojani <i.bhojani@consultant.qdsnet.com>
2026-05-09 21:28:17 +0500

    add nixpacks config

--- apps/api/src/services/cloneRepo.js
+++ apps/api/src/services/cloneRepo.js
@@ -5,16 +5,29 @@ import path from 'path'
 
 export async function cloneRepo(url) {
   const repoId = crypto.randomUUID()
+  const repoPath = path.join('/tmp', repoId)
 
-  const repoPath = path.join('./repos', repoId)
+  await fs.mkdir(repoPath, { recursive: true })
 
-  await fs.mkdir(repoPath, {
-    recursive: true
+  const match = url.match(/github\.com\/(.+?)\/(.+?)(\.git)?$/)
+  if (!match) throw new Error("Only GitHub URLs supported")
+
+  const [, owner, repo] = match
+
+  const tarUrl = `https://codeload.github.com/${owner}/${repo}/tar.gz/refs/heads/main`
+
+  const res = await fetch(tarUrl)
+
+  const filePath = path.join(repoPath, 'repo.tar.gz')
+  const fileStream = fsSync.createWriteStream(filePath)
+
+  await pipeline(res.body, fileStream)
+
+  await tar.x({
+    file: filePath,
+    cwd: repoPath,
+    strip: 1
   })
-
-  const git = simpleGit()
-
-  await git.clone(url, repoPath)
 
   return repoId
 }
